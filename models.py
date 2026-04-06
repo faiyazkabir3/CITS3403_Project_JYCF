1f2695c74571ba667ebcd476fb9ceb430100fe5b
looiyonglun-cmyk <looiyonglun@gmail.com>
2026-04-06 21:48:53 +0800

    add basic save model

--- models.py
+++ models.py
@@ -7,3 +7,14 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password_hash = db.Column(db.String(255), nullable=False)
+
+
+class SaveData(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), unique=True, nullable=False)
+    health = db.Column(db.Integer, nullable=False, default=100)
+    ammo = db.Column(db.Integer, nullable=False, default=28)
+    grenades = db.Column(db.Integer, nullable=False, default=2)
+    medkits = db.Column(db.Integer, nullable=False, default=2)
+    shield_on = db.Column(db.Boolean, nullable=False, default=True)
+    level_name = db.Column(db.String(100), nullable=False, default="start")
